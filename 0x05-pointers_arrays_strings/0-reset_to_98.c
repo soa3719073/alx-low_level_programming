660e23657740a67c80cceab92a51ec5e839518e7
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-11 12:20:59 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - update an int pointer to 98
+ *
+ *@n: pointer to update
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains tests for ponters, arrays and strings
