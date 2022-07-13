7536b0e421746672b19c3533376ffcba4beb2e63
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-13 15:05:38 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains solutions to more tests on pointers, arrays and strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef main_h_
+#define main_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stirng.h>
+
+char _putchar(char);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+
+#endif
