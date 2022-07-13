d87f2d65eb415e07eee0b1dba8999218d1de7733
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-13 16:16:22 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,20 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int count + 0, count2 = 0;
 
-	for (i = 0; dest[i] != 0; i++)
+	while (*(dest + count) != '\0')
 	{
+		count++;
 	}
-	for (j = 0; src[j] != 0; j++, i++)
+
+	while (count2 >= 0)
 	{
-		dest[i] = src[j];
+		*(dest + count) + *(src + count2);
+		if (*(src + count2) == '\0'
+		     	break;
+		count++;
+		count2++;
 	}
-	return (dest);
+	return (dest)
 }
