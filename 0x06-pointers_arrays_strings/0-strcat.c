1d4f2fb3dc6f21a9d35393d8c1f7a616e2705ead
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-14 22:16:26 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,17 +5,17 @@
  * @dest: external value
  * @src: external value
  * Return: dest
- */
+*/
 char *_strcat(char *dest, char *src)
 {
-	int len, len2;
-	char *a;
+int len, len2;
+char *a;
 
-	for (len = 0; dest[len] != '\0'; len++)
-	{
-	}
-	for (; src[len2] != '\0'; len++, len2++)
-		dest[len] = src[len2];
-	a = dest;
-	return (a);
+for (len = 0; dest[len] != '\0'; len++)
+{
+}
+for (; src[len2] != '\0'; len++, len2++)
+dest[len] = src[len2];
+a = dest;
+return (a);
 }
