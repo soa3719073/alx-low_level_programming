44615c51f6a1cb86b4e89c0ef7e768fb9cac0527
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-14 22:54:02 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,24 +2,19 @@
 
 /**
  *  _strcat - updates the value to 98.
- *
  *  @dest: external value
- *
  *  @src: external value
- *
  *  Return: dest
 */
-
 int _strlen(char *s)
 {
-	int len = 0;
+int len = 0;
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
 }
 
 /**
@@ -27,17 +22,17 @@ int _strlen(char *s)
  * @dest: copy source to this buffer
  * @src: source to copy
  * Return: concatenate strings
- */
+*/
 cchar *_strcat(char *dest, char *src)
 {
-	int i, d;
+int i, d;
 
-	i =  _strlen(dest);
+i =  _strlen(dest);
 
-	for (d = 0; src[d] != '\0'; i++, d++)
-	{
-		dest[i] = src[d];
-	}
-	dest[i] = '\0';
+for (d = 0; src[d] != '\0'; i++, d++)
+{
+dest[i] = src[d];
+}
+dest[i] = '\0';
 return (dest);
 }
