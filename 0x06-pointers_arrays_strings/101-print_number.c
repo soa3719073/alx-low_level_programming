7417659637cf8380e0fb44c368f0d4c5c6eaf9c4
soa3719073 <59306300+soa3719073@users.noreply.github.com>
2022-07-14 17:44:16 -1100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
-{
-int n;
-int a[5];
-int *p;
-
-a[2] = 1024;
-p = &n;
 /**
- * write your line of code here..
-*/
-*(p + 5) = 98\n */
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+ * print_number - print a integer
+ * @n: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int dec, count, i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * -1;
+	}
+	else
+		i = n;
+	dec = i;
+	count = 1;
+	while (dec > 9)
+	{
+		dec /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+		_putchar(((i / count) % 10) + '0');
 }
