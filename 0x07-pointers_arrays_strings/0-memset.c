189c7a4fc1a63871eface082362e57b3f2666fa0
soa3719073 <georgeadomakojnr51@gmail.com>
2022-07-20 15:06:26 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  * Description: Fill the first `n` bytes of the memory area pointed to by `s`
  * with the constant byte `b`.
  * Return: Pointer to memory area `s`
- */
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 int i;
@@ -20,6 +20,5 @@ s[i] = b;
 i++;
 n--;
 }
-
 return (s);
 }
